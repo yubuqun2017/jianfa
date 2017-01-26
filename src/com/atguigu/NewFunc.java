b8e874232c278143855fbbc46ba21edf03fd1b08
dugu <1003969758@qq.com>
2017-01-26 11:59:56 +0800

    newfunction

---
+++ src/com/atguigu/NewFunc.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+public class NewFunc {
+	
+	
+	public static void main(String[] args) {
+		System.out.println("111111111111111111111111");
+		System.out.println("111111111111111111111111");
+		System.out.println("111111111111111111111111");
+		System.out.println("111111111111111111111111");
+	}
+}
